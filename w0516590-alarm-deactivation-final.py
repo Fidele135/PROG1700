9fb36136996b08004163fb3ced50d3beed38c52f
Fidele130 <fikifedele@gmail.com>
2025-12-11 14:58:40 -0400

    update 2

--- w0516590-alarm-deactivation-final.py
+++ w0516590-alarm-deactivation-final.py
@@ -1,46 +1,53 @@
 alarm_status = "ON"
 alarm_activated_status = "ON"
-password_attempts = 3
 
-password_list = ["12345", "67890", "34567",]
-file = open("passwords.txt", "r")
-for line in file:
-    password_list.append(line.strip())
-file.close()
+def load_passwords():
+    defaults = ["12345", "67890", "34567"]
+    try:
+        with open("passwords.txt", "r") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    defaults.append(line)
+    except FileNotFoundError:
+        pass
+    return defaults
+
+password_list = load_passwords()
 
 print("Alarm Status:", alarm_status)
 print("Alarm Activated Status:", alarm_activated_status)
 
-while alarm_status == "ON" and password_attempts > 0:
+while alarm_status == "ON":
+    password_attempts = 3
 
-    print("\nEnter your 5 digit Password:")
-    user_pass = input()
+    while password_attempts > 0:
+        user_pass = input("\nEnter your 5 digit Password: ").strip()
 
-    if user_pass.isdigit() == False or len(user_pass) != 5:
-        print("Invalid Password")
-        password_attempts = password_attempts - 1
-        print("Attempts left:", password_attempts)
-        continue
+        if not user_pass.isdigit() or len(user_pass) != 5:
+            print("Invalid Password format. Use 5 digits.")
+            password_attempts -= 1
+            print("Attempts left:", password_attempts)
+            continue
 
-    if user_pass in password_list:
-        print("Password Match")
+        if user_pass in password_list:
+            print("Password Match")
+            alarm_status = "OFF"
+            alarm_activated_status = "OFF"
+            print("Alarm Status:", alarm_status)
+            print("Alarm Activated Status:", alarm_activated_status)
+            break
+        else:
+            print("Incorrect Password")
+            password_attempts -= 1
+            print("Attempts left:", password_attempts)
 
-        alarm_status = "OFF"
-        alarm_activated_status = "OFF"
-        password_attempts = 3
-
-        print("Alarm Status:", alarm_status)
-        print("Alarm Activated Status:", alarm_activated_status)
+    if alarm_status == "OFF":
         break
-    else:
-        print("Incorrect Password")
-        password_attempts = password_attempts - 1
-        print("Attempts left:", password_attempts)
-
-if password_attempts == 0:
-    alarm_status = "OFF"
-    alarm_activated_status = "ON"
-
+    
     print("\nALARM TRIGGERED!!!")
+    alarm_status = "ON"
+    alarm_activated_status = "ON"
     print("Alarm Status:", alarm_status)
     print("Alarm Activated Status:", alarm_activated_status)
+    print("\nSystem resetting... Get ready to enter your password again.\n")
